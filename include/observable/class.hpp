7beb5e2aad5fc7c51dd1a3b13a48d6b7e07179f8
Ricardo Cosme <ricardo.cosme@me.com>
2017-07-24 19:26:57 -0300

    Put `_under_transaction` and `_model` as a private state

--- include/observable/class.hpp
+++ include/observable/class.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "observable/class_member.hpp"
 #include "observable/scoped_on_change.hpp"
 #include <boost/signals2.hpp>
 
@@ -11,119 +12,11 @@ struct member
     using type = T;
     using name = M;
 };
-    
-template<typename T>
-struct get_type
-{
-    using type = typename T::type;
-};
-    
-template<typename Class, typename... Members>    
-struct class_value;
-
-template<typename Class, typename Member, typename... Members>    
-struct class_value<Class, Member, Members...> : class_value<Class, Members...>
-{
-    using Base = class_value<Class, Members...>;
-    using Base::set;
-    using Base::apply;
-    using Base::on_change;
-    
-    using T = typename Member::type;
-    using M = typename Member::name;
-
-    template<typename... Ts>
-    class_value(Class& parent, T& o, Ts&&... ts)
-        : Base(parent, std::forward<Ts>(ts)...)
-        , _parent(parent)
-        , _value(o)
-    {}
-
-    void set(M, T o)
-    {
-        _value = std::move(o);
-        if (!_parent._under_transaction)
-        {
-            _on_change(_value);
-            _parent.any_change();
-        }
-    }
-
-    const T& get() const noexcept
-    { return _value; }
-    
-    template<typename F>
-    void apply(M, F&& f)
-    {
-        f(_value);
-        if (!_parent._under_transaction)
-        {
-            _on_change(_value);
-            _parent.any_change();
-        }        
-    }
-
-    template<typename F>
-    boost::signals2::connection on_change(M, F&& f)
-    {
-        return _on_change.connect(std::forward<F>(f));
-    }
-    
-    Class& _parent;
-    T& _value;
-    boost::signals2::signal<void(T)> _on_change;
-};
-
-template<typename Class, typename Member>    
-struct class_value<Class, Member>
-{
-    using T = typename Member::type;
-    using M = typename Member::name;
-    
-    class_value(Class& parent, T& o)
-        : _parent(parent)
-        , _value(o)
-    {}
-
-    void set(M, T o)
-    {
-        _value = std::move(o);
-        if (!_parent._under_transaction)
-        {
-            _on_change(_value);
-            _parent.any_change();
-        }
-    }
-
-    const T& get() const noexcept
-    { return _value; }
-    
-    template<typename F>
-    void apply(M, F&& f)
-    {
-        f(_value);
-        if (!_parent._under_transaction)
-        {
-            _on_change(_value);
-            _parent.any_change();
-        }        
-    }
-
-    template<typename F>
-    boost::signals2::connection on_change(M, F&& f)
-    {
-        return _on_change.connect(std::forward<F>(f));
-    }
-    
-    Class& _parent;
-    T& _value;
-    boost::signals2::signal<void(const T&)> _on_change;
-};
-        
+            
 template<typename Model, typename... Members>
-struct class_ : class_value<class_<Model, Members...>, Members...>
+struct class_ : class_member<class_<Model, Members...>, Members...>
 {
-    using Base = class_value<class_<Model, Members...>, Members...>;
+    using Base = class_member<class_<Model, Members...>, Members...>;
     
     Base& as_base() noexcept
     { return static_cast<Base&>(*this); }
@@ -161,10 +54,10 @@ struct class_ : class_value<class_<Model, Members...>, Members...>
     { return _model; }
     
     boost::signals2::signal<void()> any_change;    
-// private:    
+private:    
     bool _under_transaction{false};
     Model& _model;
-private:    
+    template <typename> friend class observable::class_member;
     template <typename> friend class observable::scoped_on_change_t;
 };
 
---
+++ include/observable/class_member.hpp
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <boost/signals2.hpp>
+
+namespace observable {
+    
+template<typename T>
+struct get_type
+{
+    using type = typename T::type;
+};
+    
+template<typename Class, typename... Members>    
+struct class_member;
+
+template<typename Class, typename Member, typename... Members>    
+struct class_member<Class, Member, Members...> : class_member<Class, Members...>
+{
+    using Base = class_member<Class, Members...>;
+    using Base::set;
+    using Base::apply;
+    using Base::on_change;
+    
+    using T = typename Member::type;
+    using M = typename Member::name;
+
+    template<typename... Ts>
+    class_member(Class& parent, T& o, Ts&&... ts)
+        : Base(parent, std::forward<Ts>(ts)...)
+        , _parent(parent)
+        , _value(o)
+    {}
+
+    void set(M, T o)
+    {
+        _value = std::move(o);
+        if (!_parent._under_transaction)
+        {
+            _on_change(_value);
+            _parent.any_change();
+        }
+    }
+
+    const T& get() const noexcept
+    { return _value; }
+    
+    template<typename F>
+    void apply(M, F&& f)
+    {
+        f(_value);
+        if (!_parent._under_transaction)
+        {
+            _on_change(_value);
+            _parent.any_change();
+        }        
+    }
+
+    template<typename F>
+    boost::signals2::connection on_change(M, F&& f)
+    {
+        return _on_change.connect(std::forward<F>(f));
+    }
+    
+    Class& _parent;
+    T& _value;
+    boost::signals2::signal<void(T)> _on_change;
+};
+
+template<typename Class, typename Member>    
+struct class_member<Class, Member>
+{
+    using T = typename Member::type;
+    using M = typename Member::name;
+    
+    class_member(Class& parent, T& o)
+        : _parent(parent)
+        , _value(o)
+    {}
+
+    void set(M, T o)
+    {
+        _value = std::move(o);
+        if (!_parent._under_transaction)
+        {
+            _on_change(_value);
+            _parent.any_change();
+        }
+    }
+
+    const T& get() const noexcept
+    { return _value; }
+    
+    template<typename F>
+    void apply(M, F&& f)
+    {
+        f(_value);
+        if (!_parent._under_transaction)
+        {
+            _on_change(_value);
+            _parent.any_change();
+        }        
+    }
+
+    template<typename F>
+    boost::signals2::connection on_change(M, F&& f)
+    {
+        return _on_change.connect(std::forward<F>(f));
+    }
+    
+    Class& _parent;
+    T& _value;
+    boost::signals2::signal<void(const T&)> _on_change;
+};
+
+}
