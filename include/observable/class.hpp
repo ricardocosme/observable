dc9fc11c68a9cfe879cf0d10da712c26ac6f65b4
Ricardo Cosme <ricardo.cosme@me.com>
2017-07-25 18:55:50 -0300

    Add the template license to all source files

--- include/observable/class.hpp
+++ include/observable/class.hpp
@@ -1,3 +1,9 @@
+
+// Copyright Ricardo Calheiros de Miranda Cosme 2017.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include "observable/class_member.hpp"
